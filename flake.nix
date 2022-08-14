bf07687e81d2658611a143eeedffd20eff0d64e9
Michael Zappa <me@michzappa.com>
2022-08-14 11:59:02 -0400

    basic, barely functional template for building a kotlin gradle app with nix

---
+++ flake.nix
@@ -0,0 +1,44 @@
+{
+  description =
+    "https://en.wikibooks.org/wiki/Write_Yourself_a_Scheme_in_48_Hours";
+
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    gradle2nix.url = "github:michzappa/gradle2nix";
+    nixpkgs.url = "github:NixOs/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = { self, ... }@inputs:
+    with inputs;
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = (import nixpkgs { inherit system; });
+        buildGradle = pkgs.callPackage ./gradle-env.nix { };
+      in {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            gradle
+            inputs.gradle2nix.packages.${system}.default
+            kotlin
+            kotlin-language-server
+            ktlint
+          ];
+        };
+
+        packages.default = buildGradle {
+          pname = "kt-test";
+
+          envSpec = ./gradle-env.json;
+
+          src = ./.;
+
+          gradleFlags = [ "installDist" ];
+
+          installPhase = ''
+            mkdir -p $out
+            cp -r app/build/install/app/* $out
+          '';
+        };
+
+      });
+}
